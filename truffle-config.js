b7f1c7edaecbf18074c42299635964589fef5967
paltrickontpb <paltrickontpb@gmail.com>
2019-07-28 13:37:26 +0530

    Setup Truffle init project

---
+++ truffle-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+      gas: 5000000
+    }
+  },
+  compilers: {
+    solc: {
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200     
+        },
+      }
+    }
+  }
+};
